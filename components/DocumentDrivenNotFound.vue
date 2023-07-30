6cd01075c9f18fa63026d8b65de1039f917401c1
dima <intredford@gmail.com>
2023-07-30 15:48:23 +0800

    Добавлена страница 404

---
+++ components/DocumentDrivenNotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>
+            Страница не найдена
+        </h1>
+        <img src = "~/assets/img/0o.png">
+    </div>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+    font-weight: 700;
+    margin-bottom: 2rem;
+}
+</style>
